6881c1b50b6e2af90476030e1f8c75676d4720d4
Yuvraj210103 <ys146228@gmail.com>
2024-08-02 12:20:02 +0530

    feat: used system information lib to fetch system info and os to get os details

--- src/modules/ip_detail/ip_detail.controller.ts
+++ src/modules/ip_detail/ip_detail.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction } from "express";
 import { Request, Response } from "express";
 import DeviceDetector from "device-detector-js";
+import os from "os";
+import si from "systeminformation";
 
 export const getIpDetails = async (
   req: Request,
@@ -10,18 +12,20 @@ export const getIpDetails = async (
   try {
     const deviceDetector = new DeviceDetector();
     const userAgentString = req.headers["user-agent"];
-    const { device, os, client } = deviceDetector.parse(userAgentString || "");
+    const { client } = deviceDetector.parse(userAgentString || "");
+
+    const systemData = await si.system();
 
     // Prepare the response
     const response = {
       device: {
-        type: device?.type || "Unknown",
-        brand: device?.brand || "Unknown",
-        model: device?.model || "Unknown",
-        os: os?.name || "Unknown",
-        os_version: os?.version || "Unknown",
+        os_name: os?.type() || "Unknown",
+        os_version: os?.release() || "Unknown",
         client: client?.name || "Unknown",
         client_version: client?.version || "Unknown",
+        device_id: systemData.uuid || "unknown",
+        device_manufacturer: systemData.manufacturer || "unknown",
+        device_model: systemData.model || "unknown",
       },
     };
 
